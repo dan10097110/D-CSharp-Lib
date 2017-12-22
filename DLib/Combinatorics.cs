5ee6ede5cb15f60c80131194f8b6ca3cdc74e0ff
Dan Hog <dan24613@outlook.de>
2017-12-22 11:33:18 +0100

    combinatoric methodes now bug free

--- DLib/Combinatorics.cs
+++ DLib/Combinatorics.cs
@@ -8,50 +8,74 @@ namespace DLib
     {
         public static T[][] Combine<T>(T[] array) => Combine(array, i => true, i => i, false);
 
-        public static T[][] Combine<T>(T[] array, Func<int[], bool> CondForComb, Func<T[], T[]> ActionOnComb, bool removeDoubles)
+        public static T[][] Combine<T>(T[] array, Func<int[], bool> CondForComb, Func<int[], int[]> ActionOnComb, bool removeDoubles)
         {
-            var combs = new List<T[]>();
-            var c = new int[array.Length];
-            for (int i = 0; i < array.Length;)
+            var combs = new List<int[]>();
+            for (var c = new int[array.Length]; ;)
             {
                 if (CondForComb(c))
                 {
-                    var comb = new T[array.Length];
-                    for (int j = 0; j < comb.Length; j++)
-                        comb[j] = array[c[j]];
-                    comb = ActionOnComb(comb);
-                    if (!removeDoubles || !combs.Contains(comb))
-                        combs.Add(comb);
+                    var d = ActionOnComb(c);
+                    if (!removeDoubles || !Contains(d))
+                        combs.Add(d.ToArray());
                 }
-                c[i]++;
-                if (c[i] > array.Length)
+                c[0]++;
+                for (int i = 0; c[i] >= array.Length; c[i]++)
                 {
-                    for (int j = 0; j <= i; c[j] = 0, j++) ;
                     i++;
+                    if (i >= array.Length)
+                        return combs.Select(intToT).ToArray();
+                    for (int j = 0; j < i; c[j] = 0, j++) ;
                 }
-                else if (i > 0)
-                    i--;
             }
-            return combs.ToArray();
+
+            T[] intToT(int[] i)
+            {
+                var t = new T[array.Length];
+                for (int j = 0; j < t.Length; t[j] = array[i[j]], j++) ;
+                return t;
+            }
+
+            bool Contains(int[] i)
+            {
+                for (int j = 0; j < combs.Count; j++)
+                    if (Enumerable.SequenceEqual(i, combs[j]))
+                        return true;
+                return false;
+            }
         }
 
         public static T[][] CombineEveryLength<T>(T[] array)
         {
-            var c = new List<T[]>();
-            if (array.Length > 2)
+            if (array.Length > 1)
             {
+                var combs = new List<T[]>();
                 var a = array.ToList();
                 for (int i = 0; i < array.Length; i++)
                 {
                     a.RemoveAt(i);
-                    c.AddRange(CombineEveryLength(a.ToArray()));
+                    foreach(T[] c in CombineEveryLength(a.ToArray()))
+                    {
+                        if(!Contains())
+                            combs.Add(c.ToArray());
+
+                        bool Contains()
+                        {
+                            for (int j = 0; j < combs.Count; j++)
+                                if (Enumerable.SequenceEqual(c, combs[j]))
+                                    return true;
+                            return false;
+                        }
+                    }
                     a = array.ToList();
                 }
-                c.AddRange(Combine(array, NoDoubles, Sort, true));
+                combs.AddRange(Combine(array, NoDoubles, Sort, true));
+                return combs.ToArray();
             }
             else if (array.Length == 1)
-                c.Add(new T[] { array[0] });
-            return c.ToArray();
+                return new T[][] { new T[] { array[0] } };
+            else
+                return new T[0][];
         }
 
         public static bool NoDoubles(int[] comb)
@@ -63,7 +87,7 @@ namespace DLib
             return true;
         }
 
-        public static T[] Sort<T>(T[] comb)
+        public static int[] Sort(int[] comb)
         {
             var l = comb.ToList();
             l.Sort();
