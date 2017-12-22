5779df8c9ac2c82185baf2972639482fef06e2ce
Dan Hog <dan24613@outlook.de>
2017-12-22 12:21:46 +0100

    added faster factorisation method

--- DLib/Math/ArithmeticFunction.cs
+++ DLib/Math/ArithmeticFunction.cs
@@ -4,6 +4,6 @@ namespace DLib.Math
 {
     public static class ArithmeticFunction
     {
-        public static ulong Divisor(ulong n, ulong e) => (ulong)Combinatorics.CombineEveryLength(Prime.Factorise.Standard(n, Factor.TrialDivison)).Select(i => i.Aggregate(1, (acc, val) => acc * (int)val)).Aggregate(0, (acc, val) => acc + (int)System.Math.Pow(val, e));
+        public static ulong Divisor(ulong n, ulong e) => (ulong)Combinatorics.CombineEveryLength(Prime.Factorise.Optimised(n)).Select(i => i.Aggregate(1, (acc, val) => acc * (int)val)).Aggregate(0, (acc, val) => acc + (int)System.Math.Pow(val, e)) + 1;
     }
 }
--- DLib/Math/Prime/Factorise.cs
+++ DLib/Math/Prime/Factorise.cs
@@ -8,13 +8,35 @@ namespace DLib.Math.Prime
         public static ulong[] Standard(ulong n, Func<ulong, ulong> GetFactor)
         {
             var factors = new List<ulong>();
-            while(true)
+            while (true)
             {
                 ulong u = GetFactor(n);
-                factors.Add(u);
                 if (u == 1)
+                {
+                    factors.Add(n);
                     break;
+                }
+                factors.Add(u);
                 n /= u;
+            }
+            factors.Sort();
+            return factors.ToArray();
+        }
+
+        public static ulong[] Optimised(ulong n)
+        {
+            var factors = new List<ulong>();
+            for (; (n & 1) == 0; n >>= 1)
+                factors.Add(2);
+            for (; n % 3 == 0; n /= 3)
+                factors.Add(3);
+            for (ulong u = 5; u * u <= n; u += 2)
+            {
+                for (; n % u == 0; n /= u)
+                    factors.Add(u);
+                u += 4;
+                for (; n % u == 0; n /= u)
+                    factors.Add(u);
             }
             factors.Sort();
             return factors.ToArray();
