5fe3d768d61f434833bfcd7f125e92f8e3d68e5b
Dan Hog <dan24613@outlook.de>
2017-12-31 01:28:52 +0100

    improved ThreadQueue performance; fixed Primes bug

--- DLib/ThreadQueue.cs
+++ DLib/ThreadQueue.cs
@@ -3,7 +3,7 @@ using System.Threading;
 
 namespace DLib
 {
-    public class ThreadQueue2
+    public class ThreadQueue
     {
         Queue<string> queue = new Queue<string>();
         int threadNumber = 0;
@@ -14,32 +14,12 @@ namespace DLib
                 Thread.CurrentThread.Name = (threadNumber++).ToString();
             lock (queue)
                 queue.Enqueue(Thread.CurrentThread.Name);
-            while (queue.Peek() != Thread.CurrentThread.Name)
-                Thread.Sleep(1);
+            while (queue.Peek() != Thread.CurrentThread.Name) ;
         }
 
-        public void Finished()
+        public void Next()
         {
-            //if (Thread.CurrentThread.Name == queue.Peek())
-                lock (queue)
-                    queue.Dequeue();
-        }
-    }
-
-    public class ThreadQueue
-    {
-        Queue<string> queue = new Queue<string>();
-
-        public void Wait()
-        {
-            queue.Enqueue(Thread.CurrentThread.Name);
-            while (queue.Peek() != Thread.CurrentThread.Name)
-                Thread.Sleep(1);
-        }
-
-        public void Finished()
-        {
-            if (Thread.CurrentThread.Name == queue.Peek())
+            lock (queue)
                 queue.Dequeue();
         }
     }
