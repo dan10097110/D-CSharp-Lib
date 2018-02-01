274ac8cc52534cce690741591d445412ba331199
Dan Hog <dan24613@outlook.de>
2018-02-01 15:34:37 +0100

    Added composition

---
+++ DLib/Math/Function/Composition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DLib.Math.Function
+{
+    public class Composition : Function
+    {
+        Function a, b;
+
+        public Composition(Function a, Function b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public override double Y(double x) => a.Y(b.Y(x));
+
+        public override Function Derivate() => new Product(new Composition(a.Derivate(), b), b.Derivate());
+
+        public override Function Integrate() => throw new NotImplementedException();
+
+        public override string ToString() => "(" + a.ToString() + "Verkettung" + b.ToString() + ")";
+
+        public override Function Clone() => new Composition(a.Clone(), b.Clone());
+    }
+}
--- DLib/Math/Function/Function.cs
+++ DLib/Math/Function/Function.cs
@@ -1,5 +1,4 @@
-﻿using DLib.Math;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace DLib.Math.Function
